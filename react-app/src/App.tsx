d1c59f08d49a145c0402e50e406fe0d7d7c31006
tjshbb <scott.t.3042@gmail.com>
2024-06-26 10:15:19 -0500

    updated to include css files, also removed node-modules in root directory

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,16 +1,26 @@
 import { useState } from "react";
-import Button from "./components/Button";
-import Alert from "./components/Alert";
+// import Button from "./components/Button";
+// import Alert from "./components/Alert";
+import ListGroup from "./components/ListGroup";
+import './App.css';
 
 function App() {
-  const [alertVisible, setAlertVisibility] = useState(false);
+  // const [alertVisible, setAlertVisibility] = useState(false);
+  let items = ["New York", "San Francisco", "Tokyo", "London", "St. Louis"]
+
+  const handleSelectItem = (item: string) => {
+    console.log(item);
+  }
 
   return (
+    // <div>
+    //   {alertVisible && <Alert onClose={() => setAlertVisibility(false)}>My Alert</Alert>}
+    //   <Button onClick={() => setAlertVisibility(true)}>
+    //     <span>Alert Me</span>
+    //   </Button>
+    // </div>
     <div>
-      {alertVisible && <Alert onClose={() => setAlertVisibility(false)}>My Alert</Alert>}
-      <Button onClick={() => setAlertVisibility(true)}>
-        <span>Alert Me</span>
-      </Button>
+      <ListGroup items={items} heading="Cities" onSelectItem={handleSelectItem} />
     </div>
   );
 }
