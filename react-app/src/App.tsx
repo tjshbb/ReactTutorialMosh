215e7d30dada2bc4f4d0e5d9102e8901ad8680a0
tjshbb <scott.t.3042@gmail.com>
2024-06-26 16:55:30 -0500

    removed all previous work in app.tsx and replaced with example of how to use state to perform updates on nested structures

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,24 +1,35 @@
 import { useState } from "react";
-import Like from "./components/Like";
-import ListGroup from "./components/ListGroup";
 
 function App() {
-  const [alertVisible, setAlertVisibility] = useState(false);
-  let items = ["New York", "San Francisco", "Tokyo", "London", "St. Louis"]
 
-  const handleSelectItem = (item: string) => {
-    console.log(item);
-  }
+  const [game, setGame] = useState({
+    id: 1,
+    player: {
+      name: "John",
+    },
+  });
+
+  const [pizza, setPizza] = useState({
+    name: 'Spicy Pepperoni',
+    toppings: ['Mushroom'],
+  });
+
+  const [cart, setCart] = useState({
+    discount: .1,
+    items: [
+      { id: 1, title: 'Product 1', quantity: 1 },
+      { id: 2, title: 'Product 2', quantity: 1 },
+    ]
+  });
+
+  const handleClick = () => {
+    setGame({ ...game, player: { ...game.player, name: 'Bob' } });
+    setPizza({ ...pizza, toppings: [...pizza.toppings, 'cheese'] });
+    setCart({ ...cart, items: cart.items.map(item => item.id === 1 ? { ...item, quantity: item.quantity + 1 } : item) });
+  };
 
   return (
-    <>
-      <div>
-        <ListGroup items={items} heading="Cities" onSelectItem={handleSelectItem}></ListGroup>
-      </div>
-      <div>
-        <Like onClick={() => console.log("clicked")}></Like>
-      </div>
-    </>
+    <div></div>
   );
 }
 
