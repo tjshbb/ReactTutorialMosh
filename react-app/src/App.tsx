4aadc707157b27f5bb485ca4324ea07d6e0e66f9
tjshbb <scott.t.3042@gmail.com>
2024-06-27 09:02:12 -0500

    creaetd a navbar and cart components that interact together to provide a shopping cart example

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,11 +1,15 @@
 import { useState } from "react";
-import produce from "immer";
+import NavBar from "./components/NavBar";
+import Cart from "./components/Cart";
 
 function App() {
 
+  const [cartItems, setCartItems] = useState(['Product1', 'Product2']);
 
   return (
     <div>
+      <NavBar cartItemsCount={cartItems.length} />
+      <Cart cartItems={cartItems} onClear={() => setCartItems([])} />
     </div>
   );
 }
